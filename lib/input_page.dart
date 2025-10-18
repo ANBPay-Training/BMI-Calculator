294edd0a519f1b84e9a38c236bcfffd2da60d930
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2025-10-18 18:53:15 +0200

    indtil 129

---
+++ lib/input_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const bottomContainerColour = Color(0xFFEB1555);
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
+
+enum Gender {
+  male,
+  female,
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender? selectGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                      onPress: () {
+                        setState(() {
+                          selectGender = Gender.male;
+                        });
+                      },
+                      colour: selectGender == Gender.male
+                          ? activeCardColour
+                          : inactiveCardColour,
+                      cardChild: IconContent(
+                          icon: FontAwesomeIcons.mars, label: 'Male')),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectGender = Gender.female;
+                      });
+                    },
+                    colour: selectGender == Gender.female
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus, label: 'Feale'),
+                  ),
+                )
+              ],
+            )),
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColour,
+              ),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: activeCardColour),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                )
+              ],
+            )),
+            Container(
+              color: bottomContainerColour,
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
+        ));
+  }
+}
